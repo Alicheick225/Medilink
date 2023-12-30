b85c91bc6714b8276039c9e42ed70af96508f3d5
WOLLE JR <arnaudjuniorwolle003@gmail.com>
2023-12-30 02:17:28 +0000

    Mise a jour de la table users dans la base de donnee

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('user_name')-> nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo');
+            $table->string('phone');
+            $table->text('adresse');
+            $table->enum('role', ['admin', 'doctor', 'user'])-> default('user');
+            $table->enum('status', ['actif', 'inactif'])-> default('actif');
             $table->rememberToken();
             $table->timestamps();
         });
