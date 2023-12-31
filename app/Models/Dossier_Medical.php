19f0008c2433805fc2c4d4c2c81ac2263c6b5611
Ali <ali.ouattara2288@gmail.com>
2023-12-31 21:20:33 +0000

    Models and Migrations creation

---
+++ app/Models/Dossier_Medical.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Dossier_Medical extends Model
+{
+    protected $table = 'dossier__medicaux';
+    use HasFactory;
+}
